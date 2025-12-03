85b22711b30d08af37480cb7c9166c4f73a8f85e
Jenae Matson <jenaematson101@gmail.com>
2025-12-03 12:50:49 -0900

    Initial commit for day 4

---
+++ Day4/day4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+
+    string input;
+
+    ifstream File("inputtest.txt");
+
+    while(getline(File, input)) {
+
+    }
+
+    return 0;
+}
